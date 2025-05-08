1b4274e063bd96c04a0a88c3f003ce27de77557d
Dexzn08 <miguellamazares8@gmail.com>
2025-05-08 15:56:23 -0300

    Update alterna_animacao.py

--- scripts/alterna_animacao.py
+++ scripts/alterna_animacao.py
@@ -1,38 +1,51 @@
 import datetime
+import os
 
+# Caminho do arquivo README
 readme_path = "README.md"
 
+# Ler o conteúdo do README
+with open(readme_path, "r", encoding="utf-8") as file:
+    content = file.read()
+
+# Definir os blocos de HTML das animações
 pacman_block = '''<div id="pacman">
 <picture>
-  <source media="(prefers-color-scheme: dark)" srcset="https://raw.githubusercontent.com/miguel-lamazares/miguel-lamazares/output/pacman-contribution-graph-dark.svg">
-  <source media="(prefers-color-scheme: light)" srcset="https://raw.githubusercontent.com/miguel-lamazares/miguel-lamazares/output/pacman-contribution-graph.svg">
-  <img alt="pacman contribution graph" src="https://raw.githubusercontent.com/miguel-lamazares/miguel-lamazares/output/pacman-contribution-graph.svg">
+     <source media="(prefers-color-scheme: dark)" srcset="https://raw.githubusercontent.com/miguel-lamazares/miguel-lamazares/output/pacman-contribution-graph-dark.svg">
+     <source media="(prefers-color-scheme: light)" srcset="https://raw.githubusercontent.com/miguel-lamazares/miguel-lamazares/output/pacman-contribution-graph.svg">
+    <img alt="pacman contribution graph" src="https://raw.githubusercontent.com/miguel-lamazares/miguel-lamazares/output/pacman-contribution-graph.svg">
 </picture>
 </div>'''
 
 snake_block = '''<div id="snake" align="center">
-<img src="https://raw.githubusercontent.com/miguel-lamazares/miguel-lamazares/output/snake.svg" alt="Snake animation" />
+ <img src="https://raw.githubusercontent.com/miguel-lamazares/miguel-lamazares/output/snake.svg" alt="Snake animation" />
 </div>'''
 
-with open(readme_path, "r", encoding="utf-8") as file:
-    content = file.read()
+# Obter a hora atual
+now = datetime.datetime.now()
+current_hour = now.hour
 
-# Hora UTC
-hora = datetime.datetime.utcnow().hour
-
-# Decide qual bloco mostrar
-if hora % 4 == 0:
-    bloco = snake_block
-elif hora % 2 == 0:
-    bloco = pacman_block
+# Alternar entre as animações com base nas faixas de horário
+if current_hour >= 0 and current_hour < 2:
+    new_block = snake_block  # De 00:00 a 01:59, a animação é a da cobra
+elif current_hour >= 2 and current_hour < 4:
+    new_block = pacman_block  # De 02:00 a 03:59, a animação é o pacman
+elif current_hour >= 4 and current_hour < 6:
+    new_block = snake_block  # De 04:00 a 05:59, a animação é a da cobra
+elif current_hour >= 6 and current_hour < 8:
+    new_block = pacman_block  # De 06:00 a 07:59, a animação é o pacman
 else:
-    bloco = ""
+    new_block = snake_block  # Caso contrário, vamos alternando com a cobra (ajustar conforme necessário)
 
-# Atualiza a seção entre os marcadores
-inicio = content.find("<!--animacoes-->") + len("<!--animacoes-->")
-fim = content.find("<!--fim-animacoes-->")
+# Substituir o bloco dentro da seção <!--animacoes-->
+start_index = content.find("<!--animacoes-->") + len("<!--animacoes-->")
+end_index = content.find("<!--/animacoes-->")
 
-novo_conteudo = content[:inicio] + "\n" + bloco + "\n" + content[fim:]
+if start_index != -1 and end_index != -1:
+    content = content[:start_index] + "\n" + new_block + "\n" + content[end_index:]
+else:
+    print("A seção <!--animacoes--> não foi encontrada no README.")
 
+# Escrever o conteúdo de volta no README
 with open(readme_path, "w", encoding="utf-8") as file:
-    file.write(novo_conteudo)
+    file.write(content)
